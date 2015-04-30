61f30db534a7c0d2469c0dab2205467fdc8b0675
Ajith S <ajithsk25@users.noreply.github.com>
2015-04-30 12:55:57 +0530

    Update DataInsert.sql

--- DataInsert.sql
+++ DataInsert.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Apr 29, 2015 at 02:53 PM
+-- Generation Time: Apr 30, 2015 at 01:14 PM
 -- Server version: 5.5.40
 -- PHP Version: 5.5.22-1+deb.sury.org~precise+1
 
@@ -17,36 +17,22 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Database: `DataInsert`
+-- Database: `DataFaker`
 --
 
-DELIMITER $$
---
--- Procedures
---
-CREATE DEFINER=`root`@`localhost` PROCEDURE `GenerateFakeData`()
-BEGIN
-				DECLARE i INT DEFAULT 1;
-				WHILE i <= 10 DO
-				INSERT INTO tbl_sample_data (name, age) VALUES ((SELECT floor(rand() * 1000) AS randNum), (SELECT floor(rand() * 1000) AS randNum));
-				SET i = i + 1;
-				END WHILE;
-				END$$
-
-DELIMITER ;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tbl_sample_data`
+-- Table structure for table `tbl_data_faker`
 --
 
-CREATE TABLE IF NOT EXISTS `tbl_sample_data` (
+CREATE TABLE IF NOT EXISTS `tbl_data_faker` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(200) DEFAULT NULL,
-  `age` int(11) DEFAULT NULL,
+  `value` varchar(200) DEFAULT NULL,
+  `counter` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=101 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
