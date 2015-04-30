2717f7f757dde472acdc14623682cfe2c348658a
Ajith S <ajithsk25@users.noreply.github.com>
2015-04-30 14:44:53 +0530

    Update app.php

--- run/app.php
+++ run/app.php
@@ -6,21 +6,21 @@ use DataFaker\FakeDataInsert;
 use Nahk\PDO\SQL;
 
 if (php_sapi_name() == "cli") { // In cli-mode
-	// get parameters from terminal
-	$hostname	= $argv[1];
-	$username	= $argv[2];
-	$password  	= $argv[3];
-	$database	= $argv[4];
-	$count		= $argv[5];
-	
-	if ($hostname && $username && $password && $database && $count) { 
-		$dbConn                 = new SQL($hostname, $database, $username, $password);
-		$fakeDataInsert         = new FakeDataInsert($dbConn);
-		$fakeDataInsert->count  = $count;
-		$fakeDataInsert->insertFakeData();
-	} else {
-		echo "\n Please enter the command in terminal with following format \n\n-\t php app.php <hostname> <username> <password> <database> <limit> \n";
-	} // end if else
+    // get parameters from terminal
+    $hostname	= $argv[1];
+    $username	= $argv[2];
+    $password  	= $argv[3];
+    $database	= $argv[4];
+    $count		= $argv[5];
+    
+    if ($hostname && $username && $password && $database && $count) { 
+    	$dbConn                 = new SQL($hostname, $database, $username, $password);
+    	$fakeDataInsert         = new FakeDataInsert($dbConn);
+    	$fakeDataInsert->count  = $count;
+    	$fakeDataInsert->insertFakeData();
+    } else {
+    	echo "\n Please enter the command in terminal with following format \n\n-\t php app.php <hostname> <username> <password> <database> <limit> \n";
+    } // end if else
 } else { // Not in cli-mode
     echo "\n Could not run in browser. \n Please enter the command in terminal with following format \n\n-\t php app.php <hosnamet> <username> <password> <database> <limit> \n";
 } // end if else
